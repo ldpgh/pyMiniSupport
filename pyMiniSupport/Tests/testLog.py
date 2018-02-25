ff4db792e93bd347073161784141bd4be81cd341
notallowed7 <notallowed7>
2018-02-25 17:48:32 +0100

    short version added to testLog.py

--- pyMiniSupport/Tests/testLog.py
+++ pyMiniSupport/Tests/testLog.py
@@ -7,8 +7,6 @@ import unittest
 #........1.........2.........3.........4.........5.........6.........7.........8
 import pyMiniSupport 
 import pyMiniSupport.Log
-
-#........1.........2.........3.........4.........5.........6.........7.........8
 class TestLog(unittest.TestCase):
   def runTest(self):
     print(""); pyMiniSupport.Log.info("test of pyMiniSupport.info")
@@ -18,6 +16,18 @@ class TestLog(unittest.TestCase):
     print(""); print(pyMiniSupport.Log.prog_file())
     print(""); print(pyMiniSupport.Log.prog_file_line())
 
+#........1.........2.........3.........4.........5.........6.........7.........8
+from pyMiniSupport.Log import *
+class TestLogShort(unittest.TestCase):
+  def runTest(self):
+    print(""); info("test of pyMiniSupport.info")
+    print(""); warn("test of pyMiniSupport.warn")
+    print(""); error("test of pyMiniSupport.error")
+    print(""); print(pyMiniSupport.Log.prog_line())
+    print(""); print(pyMiniSupport.Log.prog_file())
+    print(""); print(pyMiniSupport.Log.prog_file_line())
+
 suite=unittest.TestSuite()
 suite.addTest(TestLog("runTest"))
+suite.addTest(TestLogShort("runTest"))
 unittest.TextTestRunner(verbosity=2).run(suite)
