55c6250efb8b3239f39b1baaedcefe191b9a525f
notallowed7 <notallowed7>
2018-02-25 11:55:27 +0100

    add testing

--- pyMiniSupport/Tests/testLog.py
+++ pyMiniSupport/Tests/testLog.py
@@ -2,17 +2,21 @@
 #
 # Copyright (c) 2018 ldpgh  All rights reserved
 
-from unittest import TestCase
+import unittest
 
 #........1.........2.........3.........4.........5.........6.........7.........8
 import pyMiniSupport 
 
 #........1.........2.........3.........4.........5.........6.........7.........8
-class TestLog(TestCase):
+class TestLog(unittest.TestCase):
   def test_warn(self):
     pyMinitSupport.warn("test of pyMinitSupport.warn")
     self.asserTrue(True)
   def runTest(self):
-    self.test_war()
+    self.test_warn("asdfasdfasdf")
 
-TestSuite()
+suite=unittest.TestSuite()
+test=TestLog("runTest")
+suite.addTest(test)
+testrunner=unittest.TextTestRunner(verbosity=2)
+testrunner.rn(suite)
